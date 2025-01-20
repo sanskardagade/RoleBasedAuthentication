898694540a2a000a4eb042640936e169487fad85
sanskard07 <136770535+sanskard07@users.noreply.github.com>
2025-01-20 20:36:07 +0530

    Add files via upload

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const verifyToken = require("../middlewares/authMiddleware");
+const authorizeRoles = require("../middlewares/roleMiddleware");
+const router = express.Router();
+
+//Only admins can access this router
+router.get("/admin", verifyToken, authorizeRoles("admin"), (req, res) => {
+  res.json({ message: "Welcome Admin" });
+});
+
+//Both admin and manager can access this router
+router.get(
+  "/manager",
+  verifyToken,
+  authorizeRoles("admin", "manager"),
+  (req, res) => {
+    res.json({ message: "Welcome Manager" });
+  }
+);
+//All can access this router
+
+router.get(
+  "/user",
+  verifyToken,
+  authorizeRoles("admin", "manager", "user"),
+  (req, res) => {
+    res.json({ message: "Welcome User" });
+  }
+);
+
+module.exports = router;
